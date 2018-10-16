1ec53d8ee374954a3e1bc3f9d735c0d27dc08b5e
Arnaud Lacroix <arnaud.lac@gmail.com>
2018-10-16 11:52:34 +0200

    Initial commit

---
+++ app/src/main/java/com/baocode/arnaudlacroix/gestion_contact_bda/MainActivity.java
@@ -0,0 +1,13 @@
+package com.baocode.arnaudlacroix.gestion_contact_bda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
